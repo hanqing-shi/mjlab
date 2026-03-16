f7990571da8803d2798c6a10f8f608d830108fdb
hanqing-shi <shq1847@gmail.com>
2026-03-16 00:19:03 -0700

    export policy

--- src/mjlab/tasks/tracking/tracking_env_cfg.py
+++ src/mjlab/tasks/tracking/tracking_env_cfg.py
@@ -47,7 +47,7 @@ def make_tracking_env_cfg() -> ManagerBasedRlEnvCfg:
 
   actor_terms = {
     "command": ObservationTermCfg(
-      func=mdp.generated_commands, params={"command_name": "motion"}
+      func=mdp.generated_commands, params={"command_name": "twist"}
     ),
     # "motion_anchor_pos_b": ObservationTermCfg(
     #   func=mdp.motion_anchor_pos_b,
@@ -62,7 +62,7 @@ def make_tracking_env_cfg() -> ManagerBasedRlEnvCfg:
     "base_lin_vel": ObservationTermCfg(
       func=mdp.builtin_sensor,
       params={"sensor_name": "robot/imu_lin_vel"},
-      noise=Unoise(n_min=-0.5, n_max=0.5),
+      noise=Unoise(n_min=-0, n_max=0),
     ),
     "base_ang_vel": ObservationTermCfg(
       func=mdp.builtin_sensor,
@@ -81,23 +81,25 @@ def make_tracking_env_cfg() -> ManagerBasedRlEnvCfg:
   }
 
   critic_terms = {
+    # velocity command
     "command": ObservationTermCfg(
-      func=mdp.generated_commands, params={"command_name": "motion"}
+      func=mdp.generated_commands, params={"command_name": "twist"}
     ),
+    # phase
     "motion_command": ObservationTermCfg(
-      func=mdp.motion_command, params={"command_name": "motion"}
+      func=mdp.motion_command, params={"command_name": "twist"}
     ),
     "motion_anchor_pos_b": ObservationTermCfg(
-      func=mdp.motion_anchor_pos_b, params={"command_name": "motion"}
+      func=mdp.motion_anchor_pos_b, params={"command_name": "twist"}
     ),
     "motion_anchor_ori_b": ObservationTermCfg(
-      func=mdp.motion_anchor_ori_b, params={"command_name": "motion"}
+      func=mdp.motion_anchor_ori_b, params={"command_name": "twist"}
     ),
     "body_pos": ObservationTermCfg(
-      func=mdp.robot_body_pos_b, params={"command_name": "motion"}
+      func=mdp.robot_body_pos_b, params={"command_name": "twist"}
     ),
     "body_ori": ObservationTermCfg(
-      func=mdp.robot_body_ori_b, params={"command_name": "motion"}
+      func=mdp.robot_body_ori_b, params={"command_name": "twist"}
     ),
     "base_lin_vel": ObservationTermCfg(
       func=mdp.builtin_sensor, params={"sensor_name": "robot/imu_lin_vel"}
@@ -141,7 +143,7 @@ def make_tracking_env_cfg() -> ManagerBasedRlEnvCfg:
   ##
 
   commands: dict[str, CommandTermCfg] = {
-    "motion": MotionCommandCfg(
+    "twist": MotionCommandCfg(
       entity_name="robot",
       resampling_time_range=(1.0e9, 1.0e9),
       debug_vis=True,
@@ -160,20 +162,20 @@ def make_tracking_env_cfg() -> ManagerBasedRlEnvCfg:
       anchor_body_name="",
       body_names=(),
     ),
-    "twist": UniformVelocityCommandCfg(
-      entity_name="robot",
-      resampling_time_range=(3.0, 8.0),
-      # rel_standing_envs=0.1,
-      # rel_heading_envs=0.3,
-      # heading_command=True,
-      # heading_control_stiffness=0.5,
-      debug_vis=True,
-      ranges=UniformVelocityCommandCfg.Ranges(
-        lin_vel_x=(-1.6, 1.6),
-        lin_vel_y=(-0.6, 0.6),
-        ang_vel_z=(-2.0, 2.0),
-      ), # "run":   {"vx": 2.639, "vy": 1.309, "wz": 2.008},
-    )
+    # "twist": UniformVelocityCommandCfg(
+    #   entity_name="robot",
+    #   resampling_time_range=(3.0, 8.0),
+    #   # rel_standing_envs=0.1,
+    #   # rel_heading_envs=0.3,
+    #   # heading_command=True,
+    #   # heading_control_stiffness=0.5,
+    #   debug_vis=True,
+    #   ranges=UniformVelocityCommandCfg.Ranges(
+    #     lin_vel_x=(-1.6, 1.6),
+    #     lin_vel_y=(-0.6, 0.6),
+    #     ang_vel_z=(-2.0, 2.0),
+    #   ), # "run":   {"vx": 2.639, "vy": 1.309, "wz": 2.008},
+    # )
   }
 
   ##
@@ -250,32 +252,32 @@ def make_tracking_env_cfg() -> ManagerBasedRlEnvCfg:
   rewards: dict[str, RewardTermCfg] = {
     "motion_global_root_pos": RewardTermCfg(
       func=mdp.motion_global_anchor_position_error_exp,
-      weight=3.0,
+      weight=5.0,
       params={"command_name": "motion", "std": 0.3},
     ),
     "motion_global_root_ori": RewardTermCfg(
       func=mdp.motion_global_anchor_orientation_error_exp,
-      weight=3.0,
+      weight=5.0,
       params={"command_name": "motion", "std": 0.4},
     ),
     "motion_body_pos": RewardTermCfg(
       func=mdp.motion_relative_body_position_error_exp,
-      weight=3.0,
+      weight=10.0,
       params={"command_name": "motion", "std": 0.3},
     ),
     "motion_body_ori": RewardTermCfg(
       func=mdp.motion_relative_body_orientation_error_exp,
-      weight=3.0,
+      weight=10.0,
       params={"command_name": "motion", "std": 0.4},
     ),
     "motion_body_lin_vel": RewardTermCfg(
       func=mdp.motion_global_body_linear_velocity_error_exp,
-      weight=3.0,
+      weight=10.0,
       params={"command_name": "motion", "std": 1.0},
     ),
     "motion_body_ang_vel": RewardTermCfg(
       func=mdp.motion_global_body_angular_velocity_error_exp,
-      weight=3.0,
+      weight=10.0,
       params={"command_name": "motion", "std": 3.14},
     ),
     "action_rate_l2": RewardTermCfg(func=mdp.action_rate_l2, weight=-1e-1),
