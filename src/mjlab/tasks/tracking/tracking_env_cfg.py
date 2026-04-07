35fc2d9e4600f41bedc8f5969684729fa8c16a2e
hanqing-shi <shq1847@gmail.com>
2026-04-06 22:57:26 -0700

    increase action rate reward

--- src/mjlab/tasks/tracking/tracking_env_cfg.py
+++ src/mjlab/tasks/tracking/tracking_env_cfg.py
@@ -252,35 +252,35 @@ def make_tracking_env_cfg() -> ManagerBasedRlEnvCfg:
   rewards: dict[str, RewardTermCfg] = {
     "motion_global_root_pos": RewardTermCfg(
       func=mdp.motion_global_anchor_position_error_exp,
-      weight=0.5,
+      weight=5.0,
       params={"command_name": "twist", "std": 0.3},
     ),
     "motion_global_root_ori": RewardTermCfg(
       func=mdp.motion_global_anchor_orientation_error_exp,
-      weight=0.5,
+      weight=5.0,
       params={"command_name": "twist", "std": 0.4},
     ),
     "motion_body_pos": RewardTermCfg(
       func=mdp.motion_relative_body_position_error_exp,
-      weight=1.0,
+      weight=10.0,
       params={"command_name": "twist", "std": 0.3},
     ),
     "motion_body_ori": RewardTermCfg(
       func=mdp.motion_relative_body_orientation_error_exp,
-      weight=1.0,
+      weight=10.0,
       params={"command_name": "twist", "std": 0.4},
     ),
     "motion_body_lin_vel": RewardTermCfg(
       func=mdp.motion_global_body_linear_velocity_error_exp,
-      weight=1.0,
+      weight=10.0,
       params={"command_name": "twist", "std": 1.0},
     ),
     "motion_body_ang_vel": RewardTermCfg(
       func=mdp.motion_global_body_angular_velocity_error_exp,
-      weight=1.0,
+      weight=10.0,
       params={"command_name": "twist", "std": 3.14},
     ),
-    "action_rate_l2": RewardTermCfg(func=mdp.action_rate_l2, weight=-1e-1),
+    "action_rate_l2": RewardTermCfg(func=mdp.action_rate_l2, weight=-1.0),
     "joint_limit": RewardTermCfg(
       func=mdp.joint_pos_limits,
       weight=-10.0,
